12a87a6b2a29c6803ee9e87fdec783f505faa086
Luuk Berkers <berkers.luuk@gmail.com>
2020-04-21 23:07:07 +0200

    moved to express-generator

--- app.js
+++ app.js
@@ -1,34 +1,41 @@
-#!/usr/bin/env node
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-/**
- * Copyright (C) 2020  Luuk Berkers
- *
- * This file is part of the berkers.dev
- *
- * berkers.dev is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published
- * by the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
-'use strict';
+var app = express();
 
-const express = require('express');
-const morgan = require('morgan');
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
-const app = express();
-app.use(morgan('combined'));
+app.use(logger('combined'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', function (req, res) {
-  res.send('Hello, world!');
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
 
 module.exports = app;
