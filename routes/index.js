23a41e29df222dd3b8390ac192a9ed987a829f22
Luuk Berkers <berkers.luuk@gmail.com>
2020-04-22 23:53:07 +0200

    added navbar, css, and a lot more

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  // The regex removes the trailing slash if there is one.
+  // But not here because the path is only a slash
+  // req.path.replace(/\/+$/, '')
+  res.render('index', { title: 'Express', loc: '/' });
 });
 
 module.exports = router;
