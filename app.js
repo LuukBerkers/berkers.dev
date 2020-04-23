a348515b3d83358039efbaca8dce4be56da7475c
Luuk Berkers <berkers.luuk@gmail.com>
2020-04-23 23:52:24 +0200

    added license disclaimers

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
+/**
+ * Copyright (C) 2009-2013  TJ Holowaychuk <tj@vision-media.ca>
+ * Copyright (C) 2015-2018  Douglas Christopher Wilson <doug@somethingdoug.com>
+ * Copyright (C) 2020  Luuk Berkers <berkers.luuk@gmail.com>
+ *
+ * This file is part of the berkers.dev
+ *
+ * berkers.dev is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
